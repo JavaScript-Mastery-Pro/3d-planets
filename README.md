6430c74bfeee2c7113ab59daba2c2a334e6dc82e
Hsu Win Lat <hsu@jsmastery.pro>
2024-04-05 07:36:12 +0700

    read me and code explanation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Code Review
+# 3D planets
 
+## Websites for the tetures
+
+- [SolarSystem Scope](https://www.solarsystemscope.com/textures/)
+- [3dtextures](https://3dtextures.me/)
+- [Blender guru blender tutorial](https://www.dropbox.com/sh/1rxblwi19i78ieh/AADNsHNRA7SysJ015GkXplewa?dl=0) (https://www.youtube.com/watch?v=0YZzHn0iz8U&ab_channel=BlenderGuru)
+- [Poligon - Blender Guru's website](https://www.poliigon.com/textures/free)
+
+## Have a look at EarthView.jsx for the explanation
--- src/PlanetsComponents/EarthView.jsx
+++ src/PlanetsComponents/EarthView.jsx
@@ -10,31 +10,39 @@ import { useFrame } from "@react-three/fiber";
 
 // Internal imports
 import { eb, ec, em, en, es } from "../utils";
-let mobile = window.innerWidth < 768;
+import PLanetLoader from "../components/PlanetLoader";
+let mobile = window.innerWidth < 768; // let know the mobile device
 
 const EarthView = ({ view }) => {
   const [isLoading, setIsLoading] = useState(true);
-  const earthMesh = useRef(new THREE.Mesh());
-  const cloudMesh = useRef(new THREE.Mesh());
+  const earthMesh = useRef(new THREE.Mesh()); // the earth sphere
+  const cloudMesh = useRef(new THREE.Mesh()); // the cloud sphere
 
-  const scalingFactor = Math.min(Math.max(window.innerWidth / 1200, 0.5), 1.01);
+  const scalingFactor = Math.min(Math.max(window.innerWidth / 1200, 0.5), 1.01); // auto scale accordingly to the screen size
 
   useEffect(() => {
-    earthMesh.current.visible = false;
+    earthMesh.current.visible = false; // hide the earth sphere
   }, [es, eb, ec, em, en]);
 
+  // https://threejs.org/docs/#api/en/loaders/managers/LoadingManager
+  // we will use the loading manager to know when the textures are loaded
   const manager = new THREE.LoadingManager();
   manager.onLoad = function () {
     setIsLoading(false);
     earthMesh.current.visible = true;
   };
 
+  // load the textures
+  // https://threejs.org/docs/?q=texture#api/en/loaders/TextureLoader
+  // we will use the texture loader to load the textures, and we will use the manager to know when the textures are loaded
+  // this has more control than the useTexture hook from drei
   const texture = new THREE.TextureLoader(manager).load(em);
   const normal = new THREE.TextureLoader(manager).load(en);
   const bump = new THREE.TextureLoader(manager).load(eb);
   const cloud = new THREE.TextureLoader(manager).load(ec);
   const spec = new THREE.TextureLoader(manager).load(es);
 
+  // wrap the textures with useMemo to avoid re-rendering
   useMemo(() => {
     bump.wrapS = bump.wrapT = THREE.RepeatWrapping;
     bump.repeat.setScalar(4);
@@ -47,6 +55,7 @@ const EarthView = ({ view }) => {
 
   return (
     <>
+      {/* for the whole canvas sparkles */}
       <View
         className={`w-screen h-[300vh] lg:h-[380vh] absolute -translate-x-[50%] left-[50%]`}
       >
@@ -62,6 +71,8 @@ const EarthView = ({ view }) => {
         className={`w-screen h-screen lg:w-[50vw] lg:h-[90vh] absolute -translate-x-[50%] left-[50%] top-96 z-[100] ${view}`}
       >
         <ambientLight intensity={3} />
+
+        {/* rectAreaLight is used to give the earth some light from the left side */}
         <rectAreaLight
           scale={[5, 5, 5]}
           color={"#007f5f"}
@@ -84,18 +95,19 @@ const EarthView = ({ view }) => {
           color={"#caf0f8"}
         />
         <OrbitControls
-          enableRotate={mobile ? false : true}
+          enableRotate={mobile ? false : true} // to prevent the user from rotating the earth on mobile for the scrolling
           enableZoom={false}
           rotateSpeed={0.2}
           autoRotate
           autoRotateSpeed={0.2}
-          maxPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2} // to prevent the user from rotating the earth upside down
           minPolarAngle={Math.PI / 2}
           enablePan={false}
           makeDefault
         />
         <PerspectiveCamera position={[0, 0, 0.0001]} />
-
+        {/* show the loader when the textures are loading */}
+        <PLanetLoader isLoading={isLoading} />
         <mesh
           scale={scalingFactor * 2.5}
           position={[0, scalingFactor * -0.5, 0]}
@@ -104,12 +116,12 @@ const EarthView = ({ view }) => {
         >
           <sphereGeometry args={[1, 32, 32]} />
           <meshStandardMaterial
-            map={texture}
+            map={texture} // the texture is used to give the earth its color
             metalness={0}
             roughness={1}
-            bumpMap={bump}
+            bumpMap={bump} // the bump map is used to give the earth some texture
             bumpScale={1.3}
-            normalMap={normal}
+            normalMap={normal} // the normal map is used to give the earth some depth
             normalScale={[3, 3]}
           />
         </mesh>
@@ -140,6 +152,7 @@ export const Cloud = ({ cloudMesh, map, scalingFactor }) => {
         ref={cloudMesh}
       >
         <sphereGeometry args={[1, 32, 32]} />
+        {/* https://threejs.org/docs/?q=mesh#api/en/materials/MeshPhongMaterial */}
         <meshPhongMaterial map={map} transparent={true} />
       </mesh>
     </>
